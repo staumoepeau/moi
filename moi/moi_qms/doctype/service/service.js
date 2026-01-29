c6bbd105a3fa1b9f5c14c378b63a3e22cfaee51d
Sione Taumoepeau <sione.taumoepeau@gmail.com>
2026-01-29 17:38:11 +1300

    Update MOI App Update-01

---
+++ moi/moi_qms/doctype/service/service.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Sione Taumoepeau and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Service", {
+// 	refresh(frm) {
+
+// 	},
+// });
